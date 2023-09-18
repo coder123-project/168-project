e451e176539fe35f3f35d8ce8d34e37600e17793
coder123-project <83872425+coder123-project@users.noreply.github.com>
2023-09-19 00:18:27 +0530

    Add files via upload

---
+++ js/addButtons.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent("create-buttons", {
+  init: function() {
+    // 1. Create the button
+   
+
+    // 2. Create the button
+ 
+
+    // 2. Append button elements
+   
+  }
+});
+
+
